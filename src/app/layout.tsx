7b9193d1dfc0df94208299fad7ed539c61405f15
predevho <dlagusgh7894@gmail.com>
2026-03-20 11:41:47 +0900

    a태그 Link 컴포넌트 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Link from 'next/link'
 import './globals.css'
 
 const geistSans = Geist({
@@ -30,12 +31,12 @@ export default function RootLayout({
       <body>
         <header>
           <div className="flex gap-4">
-            <a href="/" className="p-2 rounded hover:bg-gray-200">
+            <Link href="/" className="p-2 rounded hover:bg-gray-200">
               메인
-            </a>
-            <a href="/posts" className="p-2 rounded hover:bg-gray-200">
+            </Link>
+            <Link href="/posts" className="p-2 rounded hover:bg-gray-200">
               글목록
-            </a>
+            </Link>
           </div>
         </header>
         {children}
